a0277892581010ec56a1c0880870005c077ec92d
asteen24 <118776001+asteen24@users.noreply.github.com>
2023-11-26 05:53:40 -0500

    Add files via upload

---
+++ scene4.py
@@ -0,0 +1,30 @@
+# scene4.py
+
+import pygame
+from button import Button
+from statistics_page import StatisticsPage
+
+class Scene4:
+    def __init__(self, screen):
+        self.screen = screen
+        self.statistics_page = StatisticsPage(screen)
+        self.width, self.height= pygame.display.get_surface().get_size()
+
+        # Create the back button
+        self.back_button = Button(screen, "<–", 50, 50, 50, 50, (228,128,152), font_size=35, type="circle") 
+
+
+    def draw_scene(self):
+        graphimg = pygame.image.load('graphing.jpg')
+        self.screen.blit(graphimg, (0,-100))
+
+        self.back_button.draw()
+
+        self.statistics_page.draw_statistics()
+
+        
+
+    def check_button_pressed(self, point):
+        if self.back_button.check_collision(point):
+            return "Scene1"  # Switch to Scene1 when the "Back" button is clicked
+        return None
---
+++ statistics_page.py
@@ -0,0 +1,69 @@
+import pygame
+import matplotlib.pyplot as plt
+import os
+from datetime import datetime
+
+
+class StatisticsPage:
+    def __init__(self, screen):
+        self.screen = screen
+        self.width, self.height = pygame.display.get_surface().get_size()
+        self.data = []  # List to store date-based data
+        self.dates = []  # List to store dates
+
+
+        # Load data from a text file
+        self.load_data()
+
+
+    def load_data(self):
+        date_value_pairs = []  # List to store tuples of dates and values
+        if os.path.exists("data.txt"):
+            with open("data.txt", "r") as file:
+                for line in file:
+                    try:
+                        date_str, value = line.strip().split(",")
+                        date = datetime.strptime(date_str, "%x")
+                        date_value_pairs.append((date, float(value)))
+                    except ValueError as e:
+                        print(f"Error loading data: {e}. Line: {line}")
+
+
+        # Unpack date_value_pairs into separate lists
+        dates, data = zip(*date_value_pairs)
+
+
+        # Ensure that dates and data are lists
+        self.dates = list(dates)
+        self.data = list(data)
+
+
+
+    def save_data(self, date, value):
+        self.dates.append(date)
+        self.data.append(value)
+        with open("data.txt", "a") as file:
+            file.write(f"{date},{value}\n")
+        
+
+
+    def draw_statistics(self):
+        # Example line graph code
+        plt.figure(figsize=(3, 5))
+        plt.plot(self.dates, self.data, marker='o', linestyle='-')
+        plt.xlabel("Date")
+        plt.ylabel("Percent Accuracy")
+        plt.title("Percent Accuracy Over Time")
+        plt.grid()
+
+
+        # Save the graph as an image
+        plt.savefig("data_history_graph.png")
+
+
+        # Display the graph on the screen
+        graph_image = pygame.image.load("data_history_graph.png")
+        self.screen.blit(graph_image, (37.5, 100))
+
+
+        pygame.display.update()
